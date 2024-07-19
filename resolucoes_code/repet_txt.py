5ab3a3d81ae7e7ad8bb8b67fe29d82386570c277
Bianca Soares <122945054+hsoaresbianca@users.noreply.github.com>
2024-07-19 00:06:35 +0000

    resolução do tópico "2- Repetindo Textos"

--- resolucoes_code/repet_txt.py
+++ resolucoes_code/repet_txt.py
@@ -1,1 +1,13 @@
-# Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+# Agora vamos solicitar uma string e um número inteiro como entrada. Depois teremos que retornar a string repetida o número de vezes informado
+
+# Solicita uma string do usuário
+string_usuario = input("Digite uma string: ")
+
+# Solicita um número do usuário
+numero_usuario = int(input("Digite um número: "))
+
+# Repete a string pelo número informado
+resultado = (string_usuario + '') * numero_usuario
+
+# Exibe o resultado
+print("Resultado:", resultado)
